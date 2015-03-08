749cfdeebb9f9706d8705ea46ed29bd3d9b1e834
kimu_shu <alfvegardrisc@gmail.com>
2015-03-09 01:08:50 +0900

    Fix prototype and syntax mistake

--- src/altera.c
+++ src/altera.c
@@ -1,13 +1,13 @@
 #include "mruby.h"
 
-extern void altera_piocore_init(mrb_state *mrb);
+extern void altera_piocore_init(mrb_state *mrb, struct RClass *mod);
 extern void altera_piocore_final(mrb_state *mrb);
 
 void
 mrb_embed_altera_gem_init(mrb_state *mrb)
 {
   struct RClass *mod;
-  mod = mrb_define_module(mrb, "Altera")
+  mod = mrb_define_module(mrb, "Altera");
 
   altera_piocore_init(mrb, mod);
 }
